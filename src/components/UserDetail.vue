2e90481519c69c89548929c1a2a2ea2501d2f10d
Ada <adziklipa@gmail.com>
2018-05-28 20:55:25 +0100

    Initial commit

---
+++ src/components/UserDetail.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="component">
+    <h3>You may view User Details here</h3>
+    <p>Many details</p>
+    <p>User Name: {{ switchName() }}</p>
+    <p>User Age: {{ userAge }}</p>
+    <button @click="resetName">Reset Name</button>
+    <button @click="resetFn()">Reset Name</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      myName: {
+        type: String
+      },
+      resetFn: Function,
+      userAge: Number
+    },
+    methods: {
+      switchName() {
+        return this.myName.split("").reverse().join("");
+      },
+      resetName() {
+        this.myName = 'Ada';
+        this.$emit('nameWasReset', this.myName);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  div {
+    background-color: lightcoral;
+  }
+</style>
